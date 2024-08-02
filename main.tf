d0b0591a764b9235c3733881b8cfb3b9a82e7aab
ajaykumar9191 <ajaykumar91@gmail.com>
2024-08-03 01:42:23 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,22 +1,74 @@
 terraform {
   required_providers {
     snowflake = {
-      source  = "Snowflake-Labs/snowflake"
-      version = "~> 0.87"
+      source  = "chanzuckerberg/snowflake"
+      version = "0.25.17"
+    }
+  }
+
+  backend "remote" {
+    organization = "TCS_ajaykumar91"
+
+    workspaces {
+      name = "gh-actions-demo"
     }
   }
 }
 
 provider "snowflake" {
-  role = "SYSADMIN"
 }
 
-resource "snowflake_database" "db" {
-  name = "TF_DEMO"
+resource "snowflake_database" "demo_db" {
+  name    = "DEMO_DB"
+  comment = "Database for Snowflake Terraform demo"
 }
+resource "snowflake_schema" "demo_schema" {
+  database = snowflake_database.demo_db.name
+  name     = "DEMO_SCHEMA"
+  comment  = "Schema for Snowflake Terraform demo"
+}
+resource "snowflake_table" "sensor" {
+  provider = snowflake
+  database = snowflake_database.demo_db.name
+  schema   = snowflake_schema.demo_schema.name
+  name     = "WEATHER_JSON"
+  column {
+    name    = "var"
+    type    = "VARIANT"
+    comment = "Raw sensor data"
+  }
+}
+resource "snowflake_file_format" "json" {
+  provider             = snowflake
+  name                 = "JSON_FORMAT"
+  database             = snowflake_database.demo_db.name
+  schema               = snowflake_schema.demo_schema.name
+  format_type          = "JSON"
+  strip_outer_array    = true
+  compression          = "NONE"
+  binary_format        = "HEX"
+  date_format          = "AUTO"
+  time_format          = "AUTO"
+  timestamp_format     = "AUTO"
+  skip_byte_order_mark = true
+}
+resource "snowflake_stage" "example_stage" {
+  name        = "EXAMPLE_STAGE"
+  url         = "s3://snowflake-nse-data/"
+  database    = "DEMO_DB"
+  schema      = "DEMO_SCHEMA"
+  credentials =  "AWS_KEY_ID='${var.access_key}' AWS_SECRET_KEY='${var.secret_key}'"
+}
+resource "snowflake_view" "view" {
+  database = "DEMO_DB"
+  schema   = "DEMO_SCHEMA"
+  name     = "NEW_VIEW"
 
-resource "snowflake_warehouse" "warehouse" {
-  name           = "TF_DEMO"
-  warehouse_size = "xsmall"
-  auto_suspend   = 60
+  comment = "comment"
+
+  statement  = <<-SQL
+    select * from WEATHER_JSON;
+SQL
+  or_replace = false
+  is_secure  = false
 }
